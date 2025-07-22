f86b5539d4a9743657661a049463e80124179ec2
faulkness <johnfaulkner45@yahoo.com>
2025-07-21 22:15:41 -0400

    Delete build_scripts/build_installer.bat

--- build_scripts/build_installer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM ensure output folder exists
-if not exist build mkdir build
-
-REM export the Godot project as a Windows .exe
-godot --export "Windows Desktop" build\LegendOfDragoon_Disc1.exe
